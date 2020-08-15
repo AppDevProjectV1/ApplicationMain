adeb0c0c48ef1098908654fc88b26037a25f1c7f
Mayanklna <mp410081@gmail.com>
2020-08-15 11:27:23 +0530

    Intial,login,number,prof,getstartactivity

---
+++ app/src/main/java/com/example/appdevproject/NumberRegisterActivity.java
@@ -0,0 +1,18 @@
+package com.example.appdevproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
+
+public class NumberRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_number_register);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/appdevproject/Splash1Activity.java
@@ -0,0 +1,39 @@
+package com.example.appdevproject;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Splash1Activity extends AppCompatActivity {
+
+    private ImageView logo;
+    private static int splashTimeOut=1200;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash1);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(Splash1Activity.this,NumberRegisterActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },splashTimeOut);
+
+
+
+    }
+}
